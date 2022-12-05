a43381061583db54c2f64c45baf42b52592fafd4
mirzapm13 <mirzapm13@gmail.com>
2022-12-05 07:38:13 +0700

    webpack-react-setup

--- srctest/components/Typography/index.jsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react/jsx-props-no-spreading */
-/* eslint-disable react/prop-types */
-import React from 'react';
-
-function Typography({ children, ...rest }) {
-  return <h1 {...rest}>{children}</h1>;
-}
-export default Typography;
